37e27fe3f9f2a456d19f412ae6db0a562a5dc4dd
Sean McGregor <sean@syntiant.com>
2021-02-06 15:18:06 -0800

    added hello world of newsplease

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
 echo "Hello $1"
+python newsplease.py
 time=$(date)
 echo "::set-output name=time::$time"
---
+++ newsplease.py
@@ -0,0 +1,3 @@
+from newsplease import NewsPlease
+article = NewsPlease.from_url('https://seanbmcgregor.com/DeepfakeDetectionGame.html')
+print(article.title)
