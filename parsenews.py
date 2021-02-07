0c845217babeda16053b17b6fe836da54b035137
Sean McGregor <sean@syntiant.com>
2021-02-07 01:00:37 -0800

    updated parsenews hello world

--- parsenews.py
+++ parsenews.py
@@ -4,10 +4,10 @@ import json
 from newsplease import NewsPlease
 
 def respond(err, res=None):
-    article = NewsPlease.from_url('https://seanbmcgregor.com/DeepfakeDetectionGame.html')
+    article = json.dumps(NewsPlease.from_url('https://seanbmcgregor.com/DeepfakeDetectionGame.html').get_dict(), default=str)
     return {
         'statusCode': '400' if err else '200',
-        'body': err.message if err else json.dumps(article),
+        'body': err.message if err else article,
         'headers': {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Headers': 'Content-Type',
@@ -21,6 +21,7 @@ def lambda_handler(event, context):
     access to the request and response payload, including headers and
     status code.
     '''
+    return respond(None, {})
     operation = event['httpMethod']
     if operation in ['GET', 'POST', 'PUT', 'OPTIONS']:
         payload = event['queryStringParameters'] if operation == 'GET' else json.loads(event['body'])
