f4007a51db887ee45d2cff2948d88d67162523ea
Sean McGregor <git.commit@seanbmcgregor.com>
2021-02-08 00:15:00 -0800

    simplified endpoint

--- parsenews.py
+++ parsenews.py
@@ -3,18 +3,6 @@
 import json
 from newsplease import NewsPlease
 
-def respond(err, res=None):
-    return {
-        'statusCode': '400' if err else '200',
-        'body': err.message if err else res,
-        'headers': {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-        },
-    }
-
 def lambda_handler(event, context):
     '''Support the HTTP CORS pre-flight, then respond to the actual request.
     '''
@@ -25,7 +13,7 @@ def lambda_handler(event, context):
         res = json.dumps(
             NewsPlease.from_url(event['url']).get_dict(),
             default=str)
-        return respond(None, res)
+        return res
     else:
         context.log('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy!')
-        return respond(None, {})
+        return "returning nothing"
