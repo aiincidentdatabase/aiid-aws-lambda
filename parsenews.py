d53ddf62a6c49b649d22aadda8326ffee2ab419b
Sean McGregor <git.commit@seanbmcgregor.com>
2021-02-07 23:56:52 -0800

    added debug statements

--- parsenews.py
+++ parsenews.py
@@ -18,10 +18,14 @@ def respond(err, res=None):
 def lambda_handler(event, context):
     '''Support the HTTP CORS pre-flight, then respond to the actual request.
     '''
+    context.log('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz!')
     if 'url' in event:
+        context.log('2222222222222222!')
+        context.log(event['url'])
         res = json.dumps(
             NewsPlease.from_url(event['url']).get_dict(),
             default=str)
         return respond(None, res)
     else:
+        context.log('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy!')
         return respond(None, {})
