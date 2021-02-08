bb001bcb732530bfc52d95bd937b78b8502363d6
Sean McGregor <git.commit@seanbmcgregor.com>
2021-02-08 01:11:53 -0800

    potential fix

--- parsenews.py
+++ parsenews.py
@@ -9,11 +9,12 @@ def lambda_handler(event, context):
     context.log('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz!')
     context.log(event)
     context.log(str(event.keys()))
-    if 'url' in event:
+    if 'body' in event:
         context.log('2222222222222222!')
-        context.log(event['url'])
+        context.log(event['body'])
+        context.log(event['body']['url'])
         res = json.dumps(
-            NewsPlease.from_url(event['url']).get_dict(),
+            NewsPlease.from_url(event['body']['url']).get_dict(),
             default=str)
         return res
     else:
